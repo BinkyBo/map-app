ca62179aa502cf23120f87c567d46cdceabdeef3
BinkyBo <wmarshall098@gmail.com>
2025-11-05 17:44:08 +0000

    initial

---
+++ frontend/src/app.jsx
@@ -0,0 +1,10 @@
+import MapView from "./MapView";
+
+export default function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Map Message Board</h2>
+      <MapView />
+    </div>
+  );
+}
